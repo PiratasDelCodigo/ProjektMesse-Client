7d6094dd89f8b20b209ce758e8105e557651d2f5
m-ancev <m.ancev04@gmail.com>
2024-11-13 11:50:08 +0100

    Implement Admin Model

---
+++ Models/Admin.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messe_Client.Models
+{
+    public class Admin
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Password { get; set; }
+    }
+}
