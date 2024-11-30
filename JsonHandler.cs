d09a27f5c1950089345be3beac0bd19cd2f5a792
maxi2708 <34394009+maxi2708@users.noreply.github.com>
2024-11-30 14:54:16 +0100

    added JsonHandler for proper OfflineHandling

---
+++ JsonHandler.cs
@@ -0,0 +1,80 @@
+﻿using Messe_Client.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Messe_Client
+{
+    internal static class JsonHandler
+    {
+        public static dynamic? jsonData { get; set; }
+
+        public static (Company[]?, DateTime) getCompanyFromJSON()
+        {
+            string json = File.ReadAllText("data.json");
+            jsonData = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+            jsonData["currentTimestamp"] = DateTime.UtcNow;
+            File.WriteAllText("data.json", jsonData.ToString());
+            var companies = jsonData!["companies"].ToObject<Company[]>();
+            var timestamp = jsonData["currentTimestamp"].ToObject<DateTime>();
+            return (companies, timestamp);
+        }
+
+        public static (Customer[]?, DateTime) getCustomersFromJSON()
+        {
+            string json = File.ReadAllText("data.json");
+            jsonData = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+            jsonData["currentTimestamp"] = DateTime.UtcNow;
+            File.WriteAllText("data.json", jsonData.ToString());
+            var customers = jsonData!["customers"].ToObject<Customer[]>();
+            var timestamp = jsonData["currentTimestamp"].ToObject<DateTime>();
+            return (customers, timestamp);
+        }
+
+        public static (ProductGroup[]?, DateTime) getProductGroupsFromJSON()
+        {
+            string json = File.ReadAllText("data.json");
+            jsonData = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+            jsonData["currentTimestamp"] = DateTime.UtcNow;
+            File.WriteAllText("data.json", jsonData.ToString());
+            var productGroups = jsonData!["productGroups"].ToObject<ProductGroup[]>();
+            var timestamp = jsonData["currentTimestamp"].ToObject<DateTime>();
+            return (productGroups, timestamp);
+        }
+
+        public static void setCompaniesToData(string companyJSON)
+        {
+            string json = File.ReadAllText("data.json");
+            jsonData = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+            dynamic companies = Newtonsoft.Json.JsonConvert.DeserializeObject(companyJSON);
+            jsonData["companies"] = companies;
+            jsonData["currentTimestamp"] = DateTime.UtcNow;
+            File.WriteAllText("data.json", jsonData.ToString());
+        }
+
+        public static void setCustomersToData(string companyJSON)
+        {
+            string json = File.ReadAllText("data.json");
+            jsonData = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+            dynamic companies = Newtonsoft.Json.JsonConvert.DeserializeObject(companyJSON);
+            jsonData["customers"] = companies;
+            jsonData["currentTimestamp"] = DateTime.UtcNow;
+            File.WriteAllText("data.json", jsonData.ToString());
+        }
+
+        public static void setProductGroupToData(string companyJSON)
+        {
+            string json = File.ReadAllText("data.json");
+            jsonData = Newtonsoft.Json.JsonConvert.DeserializeObject(json);
+            dynamic companies = Newtonsoft.Json.JsonConvert.DeserializeObject(companyJSON);
+            jsonData["productGroups"] = companies;
+            jsonData["currentTimestamp"] = DateTime.UtcNow;
+            File.WriteAllText("data.json", jsonData.ToString());
+        }
+
+    }
+
+}
